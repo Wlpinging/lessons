d442599f183ccd9f293430445fc5c461301f5e7d
lieping <wlping_ing@126.com>
2019-12-10 09:49:24 +0800

    lesson 1 with DFU function in ble_Uart

---
+++ pca10040/s132/ses/Output/ble_app_uart_pca10040_s132 Release/Obj/system_nrf52.d
@@ -0,0 +1,17 @@
+Output/ble_app_uart_pca10040_s132\ Release/Obj/system_nrf52.o: \
+ C:\Users\William\Downloads\nRF5_SDK_16.0.0_98a08e2\modules\nrfx\mdk\system_nrf52.c \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.30a/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.30a/include/stdbool.h \
+ C:\Users\William\Downloads\nRF5_SDK_16.0.0_98a08e2\modules\nrfx\mdk\nrf.h \
+ C:\Users\William\Downloads\nRF5_SDK_16.0.0_98a08e2\modules\nrfx\mdk\nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ C:\Users\William\Downloads\nRF5_SDK_16.0.0_98a08e2\modules\nrfx\mdk\system_nrf52.h \
+ C:\Users\William\Downloads\nRF5_SDK_16.0.0_98a08e2\modules\nrfx\mdk\system_nrf.h \
+ C:\Users\William\Downloads\nRF5_SDK_16.0.0_98a08e2\modules\nrfx\mdk\nrf52_bitfields.h \
+ C:\Users\William\Downloads\nRF5_SDK_16.0.0_98a08e2\modules\nrfx\mdk\nrf51_to_nrf52.h \
+ C:\Users\William\Downloads\nRF5_SDK_16.0.0_98a08e2\modules\nrfx\mdk\nrf52_name_change.h \
+ C:\Users\William\Downloads\nRF5_SDK_16.0.0_98a08e2\modules\nrfx\mdk\compiler_abstraction.h
